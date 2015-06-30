10b989771640f29f6a529ef82dfe1fa16b0a7902
Essam Al Joubori <essam.joubori@gmail.com>
2015-06-30 15:23:38 -0400

    Add routes

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  entry: './app/components/Main.js',
-  ouput:  {
-    filename: 'public/bundle.js'
+  entry: "./app/App.js",
+  output: {
+    filename: "public/bundle.js"
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules | bower_components)/,
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel'
       }
     ]
