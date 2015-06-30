dd4ba244039f0993332763d7d59aac315e374d93
Essam Al Joubori <essam.joubori@gmail.com>
2015-06-30 14:51:57 -0400

    Add webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: './app/components/Main.js',
+  ouput:  {
+    filename: 'public/bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules | bower_components)/,
+        loader: 'babel'
+      }
+    ]
+  }
+};
